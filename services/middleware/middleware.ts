ea6943906d7850f6406f6c2b96408db584beb2f8
SameerAther <unknowncoder769@gmail.com>
2024-08-13 18:50:25 +0500

    fix: Env and minor bug fixed

--- services/middleware/middleware.ts
+++ services/middleware/middleware.ts
@@ -5,10 +5,10 @@ const getAccessToken = () => {
 };
 
 const api = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   // baseURL: "https://trade-prod.yameenyousuf.com/api",
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://trade.yameenyousuf.com/api",
+  // baseURL: "https://trade.yameenyousuf.com/api",
   // baseURL: 'http://128.199.30.51:8080/api'
 });
 

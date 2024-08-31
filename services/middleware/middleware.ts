6734efff0c23f1a51e3eaa16334d4580fc9ce047
SameerAther <unknowncoder769@gmail.com>
2024-09-01 04:59:12 +0500

    fix: env issue fixed

--- services/middleware/middleware.ts
+++ services/middleware/middleware.ts
@@ -4,12 +4,15 @@ const getAccessToken = () => {
   return localStorage.getItem("accessToken");
 };
 
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+console.log(BASE_URL, "BASE_URL");
 const api = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL:
+    BASE_URL === undefined ? "https://trade.yameenyousuf.com/api" : BASE_URL,
   // baseURL: "https://trade-prod.yameenyousuf.com/api",
 
   // baseURL: "http://localhost:8080/api",
-  baseURL: "https://trade.yameenyousuf.com/api",
+  //baseURL: "https://trade.yameenyousuf.com/api",
 
   // baseURL: "http://128.199.30.51:8080/api",
 });

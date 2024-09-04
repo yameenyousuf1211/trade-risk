6fbebace27a24b27cab89ff6d5d78d2fd9b60a42
SameerAther <unknowncoder769@gmail.com>
2024-09-04 22:23:02 +0500

    fix: Prod draft hotfix

--- app/create-request/discount/page.tsx
+++ app/create-request/discount/page.tsx
@@ -178,8 +178,9 @@ const CreateDiscountPage = () => {
       reqData = {
         ...rest,
         ...baseData,
-        draft: true,
+        draft: "true",
       };
+      console.log(reqData, "reqData Discounting");
       reqData.currency = reqData.currency ?? "USD";
       setLoader(true);
       const { response, success } = discountingData?._id

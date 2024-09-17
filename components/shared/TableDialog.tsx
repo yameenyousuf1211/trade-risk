2ec13839d87c3815f895c490c81a6af0925b1acc
SameerAther <unknowncoder769@gmail.com>
2024-09-18 01:12:10 +0500

    fix: Minor fixes

--- components/shared/TableDialog.tsx
+++ components/shared/TableDialog.tsx
@@ -568,7 +568,7 @@ export const TableDialog = ({
                       value={
                         lcData?.paymentTerms &&
                         lcData?.paymentTerms !== "Sight LC"
-                          ? `${lcData.paymentTerms} for ${
+                          ? `${lcData.paymentTerms} - ${
                               lcData.extraInfo?.days + " days" || ""
                             } at ${lcData.extraInfo?.other || ""}`
                           : lcData?.paymentTerms || "-"
